dce37dfde520781e8895cb00717e128766b34dc5
hozl03 <hozl-wp21@student.tarc.edu.my>
2024-09-15 17:11:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,6 +97,8 @@ scaler = StandardScaler()
 X[important_num_cols] = scaler.fit_transform(X[important_num_cols])
 
 X.head()
+st.write(X.head())
+st.wrtite(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
